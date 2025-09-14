7612c3521379cd1d4bb13dac907326e442ce402b
souledout326-stack <souledout326@gmail.com>
2025-09-15 00:35:14 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,18 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('fitness-365-cache').then(cache => {
+      return cache.addAll([
+        'index.html',
+        'style.css',
+        'manifest.json',
+        'icon.png'
+      ]);
+    })
+  );
+});
 
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
